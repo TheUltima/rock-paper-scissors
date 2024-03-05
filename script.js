5d36b5bf2d692d788eefa22f69f599488129a5a1
TheUltima <xxdeatjerzxx@gmail.com>
2024-03-04 23:14:04 -0500

    Give rest of move buttons eventListeners
    no problems found currently

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ function getComputerChoice() {
 function playRound(playerSelection) {
   let playerMove = playerSelection;
   let compMove = getComputerChoice();
-  console.log(playerMove);
 
   if (playerMove === compMove) {
     console.log(`It was a tie since you both picked ${playerMove}`);
@@ -50,13 +49,38 @@ function playRound(playerSelection) {
   }
 }
 
+function playRound2(playerSelection) {
+  const playerMove = playerSelection;
+  let compMove = getComputerChoice();
+
+  if (playerMove === compMove) {
+    console.log(`It was a tie since you both picked ${playerMove}`);
+    return;
+  }
+
+  if (RPS_rules[playerMove][compMove]) {
+    console.log(
+      `You picked ${playerMove}, while the computer picked ${compMove}. You win!!`
+    );
+  } else {
+    console.log(
+      `You picked ${playerMove}, while the computer picked ${compMove}. You lose`
+    );
+  }
+}
+
+//bind() method allows us to pass parameters to callback functions that have them.
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", playRound.bind(null, "rock"));
 
 const paper = document.querySelector("#paper");
+paper.addEventListener("click", playRound.bind(null, "paper"));
 
 const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", playRound.bind(null, "scissors"));
 
 const lizard = document.querySelector("#lizard");
+lizard.addEventListener("click", playRound.bind(null, "lizard"));
 
 const spock = document.querySelector("#spock");
+spock.addEventListener("click", playRound.bind(null, "spock"));
